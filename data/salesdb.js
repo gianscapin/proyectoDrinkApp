7129db38adbcea77a70165bbe39e3cd19e8fd4ea
nicoRoldan128 <nicolasroldan128@gmail.com>
2021-06-09 19:20:27 -0300

    agrego id

--- data/salesdb.js
+++ data/salesdb.js
@@ -11,7 +11,7 @@ const getSales = async ()=>{
     return sales
 }
 
-const getSale = async ()=>{
+const getSale = async ()=>{ 
     const clientMongo = await connection.getConnection()
 
     const sale = await clientMongo.db('drinkApp').collection('sales').findOne({_id:new objectId(id)})
